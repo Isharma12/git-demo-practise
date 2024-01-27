6b522653f80e853e0d03bfd4e1840c2489bc4597
Isharma12 <indush8412@gmail.com>
2024-01-27 15:14:27 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Indu", "Hello");
+        System.out.println("My name is Indu, Hello");
         System.out.println("Prime Testing");
         
     }
