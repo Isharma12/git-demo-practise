dffc41c4a9716e7fbf8126e34d85a42be9a519ad
Isharma12 <147100104+Isharma12@users.noreply.github.com>
2024-01-27 15:12:41 +0000

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Indu");
+        System.out.println("My name is Indu", "Hello");
         System.out.println("Prime Testing");
+        
     }
 }
