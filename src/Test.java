42a48f2e54fdfac31a621b1709e5281d97968add
Happy-Tester <146895182+Nik-prajap@users.noreply.github.com>
2024-01-27 15:16:51 +0000

    commit9

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,13 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("My name is Indu, Hello");
         System.out.println("Prime Testing");
+        System.out.println("            **");
+            System.out.println("    **              **");
+            System.out.println("**      **      **      **");
+            System.out.println("    **      **      **");
+            System.out.println("**      **      **      **");
+            System.out.println("    **              **");
+            System.out.println("            **");
         
     }
 }
